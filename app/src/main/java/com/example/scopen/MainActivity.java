7ece951da9e22c7e80d1509051f979d39c91b9fa
jefflongo <jeff.a.longo@gmail.com>
2020-05-25 01:26:53 -0700

    first pass at live plot

--- app/src/main/java/com/example/scopen/MainActivity.java
+++ app/src/main/java/com/example/scopen/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.scopen;
 
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -11,45 +21,108 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MAX_SAMPLES = 10;
+    private static final int BG_COLOR = 0xFF47474E;
+    private static final int LINE_COLOR = 0xFFFFFACD;
+
+    private int mTimeStep = 1;
+
+    private LineChart mChart;
+    private LineDataSet mDataSet;
+    private LineData mLineData;
+
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Configure graph
+        mChart = (LineChart) findViewById(R.id.chart);
+        mChart.setHardwareAccelerationEnabled(true);
+        mChart.setBackgroundColor(BG_COLOR);
+        mChart.setDescription(null);
+        mChart.setAutoScaleMinMaxEnabled(false);
+        mChart.setPinchZoom(false);
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setTouchEnabled(false);
+
+        // Disable legend
+        Legend legend = mChart.getLegend();
+        legend.setEnabled(false);
+
+        // Configure y-axis left
+        YAxis yAxisLeft = mChart.getAxisLeft();
+        yAxisLeft.setDrawGridLines(true);
+        yAxisLeft.setDrawAxisLine(false);
+        yAxisLeft.setDrawLabels(true);
+        //yAxisLeft.enableGridDashedLine(10, 0, 0);
+
+        // Configure y-axis right
+        YAxis yAxisRight = mChart.getAxisRight();
+        yAxisRight.setDrawGridLines(true);
+        yAxisRight.setDrawAxisLine(false);
+        yAxisRight.setDrawLabels(false);
+        yAxisRight.setEnabled(false);
+        //yAxisRight.enableGridDashedLine(10, 10, 0);
+
+        // Configure x-axis
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setDrawGridLines(true);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawLabels(false);
+        //xAxis.enableGridDashedLine(10, 10, 0);
+
+        // Configure data
+        mDataSet = new LineDataSet(null, null);
+        mDataSet.setDrawValues(false);
+        mDataSet.setColor(LINE_COLOR);
+        mDataSet.setCircleColor(LINE_COLOR);
+        mDataSet.setLineWidth(3f);
+        mDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        mLineData = new LineData(mDataSet);
+        mChart.setData(mLineData);
+        mChart.invalidate();
+
+        Thread thread = new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                while (true) {
+                    addEntry(mLineData.getEntryCount() * mTimeStep, new Random().nextInt(10));
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
+
+        thread.start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void addEntry(final float t, final float v) {
+        if (mDataSet.getEntryCount() == MAX_SAMPLES) {
+            mDataSet.removeFirst();
+            for (Entry entry : mDataSet.getValues()) {
+                entry.setX(entry.getX() - mTimeStep);
+            }
         }
-
-        return super.onOptionsItemSelected(item);
+        mDataSet.addEntry(new Entry(t, v));
+        mDataSet.notifyDataSetChanged();
+        mLineData.notifyDataChanged();
+        mChart.notifyDataSetChanged();
+        mChart.invalidate();
     }
 }
